57a62e12962d74834962c6f08538ae7ad542da0d
qetqetqet <69961099+qetqetqet@users.noreply.github.com>
2024-03-26 02:10:16 +0800

    Updated files with corrections

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8onb+ie23fo^6)#e=rao(dzu=w*_m3anfn1^3w514^g$2tt1#l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
